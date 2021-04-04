b20bcedc2776200784c2a0630124296c0df18a0b
kalabovanikola <nikola@kalabova.eu>
2021-04-04 23:42:30 +0200

    nikolic index, comments

--- calculate_indices.py
+++ calculate_indices.py
@@ -22,51 +22,58 @@ class Connectivity:
     def calculate_connectivities(G,f,var=1):
         return sum(f(G.degree(e[0]),G.degree(e[1]))**var for e in G.edges)
 
-    
+    # Zagreb index
     def zagreb(G):
         m1 = sum((G.degree(i))**2 for i in range(len(G)))
         m2 = Connectivity.calculate_connectivities(G, lambda a, b : a * b)
         return m1, m2
     
     
+    # Zagreb index as modified by Nikolić
     def modified_zagreb(G):
         m1 = sum(1/((G.degree(i))**2) for i in range(len(G)))
         m2 = Connectivity.calculate_connectivities(G, lambda a, b : 1/(a * b))
         return m1, m2
 
 
+    # Variable zagreb index as proposed by Milicević and Nikolić
     def variable_zagreb(G,var):
         m1 = sum((G.degree(i)**var) for i in range(len(G)))
         m2 = Connectivity.calculate_connectivities(G, lambda a, b : (a * b), var)
         return m1, m2
     
-    
+    # Randić connectivity index
     def randic(G):
         return Connectivity.calculate_connectivities(G, lambda a, b : 1/math.sqrt((a * b)))
     
     
+    # Sum connectivity index
     def sum_connectivity(G):
         return Connectivity.calculate_connectivities(G, lambda a, b : 1/math.sqrt((a + b)))
     
     
+    # General Randić connectivity index
     def general_randic(G,var):
         return Connectivity.calculate_connectivities(G, lambda a, b : (a * b), var)
     
     
+    # General sum-connectivity index
     def general_sum_connectivity(G,var):
         return Connectivity.calculate_connectivities(G, lambda a, b : (a + b), var)
     
     
 class Path:
     
-       
+    
+    # Balan J
     def balaban(G):
         distances = [sum(nx.shortest_path_length(G, source=i).values()) for i in range(len(G))]
         balaban =  sum(math.sqrt(distances[e[0]]*distances[e[1]]) for e in G.edges)
         balaban *= (G.number_of_edges()/(G.number_of_edges()-len(G)+2))
         return balaban
             
-        
+    
+    # Szeged index as proposed by Gutman
     def szeged(G):
         szeged = 0
         for e in G.edges:
@@ -83,6 +90,7 @@ class Path:
         return szeged
         
         
+    # Revised szeged index (also called revised wiener index), by Randić
     def revised_szeged(G):
         szeged = 0
         for e in G.edges:
@@ -102,6 +110,7 @@ class Path:
         return szeged
         
         
+    # Detour index by Lukovits
     def detour(G):
         detour = 0
         for i in range(len(G)):
@@ -110,14 +119,25 @@ class Path:
         return detour
 
 
+    # Harary index by Plavšić
     def harary(G):
         return sum(sum(1/x for x in v_lenghts.values() if x>0) for v_lenghts in dict(nx.shortest_path_length(G)).values())/2
 
-        
+
+    # Wiener index    
     def wiener(G):
         return nx.average_shortest_path_length(G, weight='weight')* len(G)*(len(G)-1) * 0.5
 
 
+    # Nikolic modification of wiener index on trees
+    def nikolic(G):
+        if not nx.is_tree(G):
+            print("G is not a tree")
+            return
+        return sum(reduce((lambda x, y: 1/(x * y)), (len(Matching.remove_edge(G,e).subgraph(c)) for c in nx.connected_components(Matching.remove_edge(G,e)))) for e in G.edges)
+    
+
+    # Graovac-pisanski index
     def pisanski(G):
         GM = algorithms.isomorphism.GraphMatcher(G, G)
         paths_len = 0
@@ -137,26 +157,30 @@ class Walk:
         return paths
     
     
+    # Path-walk ratio 1; proposed by Randić
     def path_walk_ratio1(G,k):
         walks =  matrix_power(nx.to_numpy_matrix(G), k).sum(axis=1)
         paths = [all_paths_from_v(G,v) for v in range(len(G))]
         return sum(paths[i]/walks[i[0]] for i in range(len(G)))
     
 
+    # Path-walk ratio 2
     def path_walk_ratio2(G,k):
         walks = matrix_power(nx.to_numpy_matrix(G), k).sum()
         paths = sum(all_paths_from_v(G,v) for v in range(len(G)))
         return paths/walks 
     
     
-    def number_of_patks_k(G,k):
+    def number_of_paths_k(G,k):
         return sum(all_paths_from_v(G,v) for v in range(len(G)))
     
     
+    # Estrada index proposed by estrada
     def estrada(G,k):
         return matrix_power(nx.to_numpy_matrix(G), k).trace()
         
         
+    # Index based on the markov matrix (Klein et al.)
     def markov(G,k):
         m = np.array(nx.to_numpy_matrix(G))
         m = np.matrix([[1/(sum(m[i])) if m.item(i,j)==1.0 else 0 for j in range(len(m[i]))] for i in range(len(m))])
@@ -194,6 +218,7 @@ class Matching:
         return reduce((lambda x, y: x * y), (len(G.subgraph(c)) for c in nx.connected_components(G)))
         
     
+    # Hosoya index (not the most effective implementation)
     def hosoya(G):
         #connected component with the most edges
         K = max([G.subgraph(c).copy() for c in nx.connected_components(G)],key=lambda x: len(x.edges()))
@@ -206,6 +231,7 @@ class Matching:
         return m1 + m2
     
     
+    # Wiener-Hosoya index, proposed by Randić
     def wiener_hosoya(G):
         w1 = Path.wiener(G)
         return w1 + sum(0 if (G.degree(e[0]) == 1 or G.degree(e[1]) == 1) else Matching.calculate_wiener_hosoya(Matching.remove_edges(G,e)) for e in G.edges)
